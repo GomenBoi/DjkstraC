88acf56e9d2f42fbf85d766a3b62d5af464c0ae2
GomenBoi <dasilvaroberto385@yahoo.com>
2023-11-22 11:59:57 +0000

    organised files

---
+++ main/Djkstra.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "AdjacencyMatrix.c"
+
+
+int main(int argc, char** argv) {
+    AdjacencyMatrix * p;
+
+    p = initialise_matrix(5);
+    
+    return 0;
+}   
